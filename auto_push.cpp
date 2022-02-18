72a042a80261f9d3d64563af1cfa715aec619f47
Ophen <itsOphen@gmail.com>
2022-02-18 15:20:49 +0100

    haha

--- auto_push.cpp
+++ auto_push.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <unistd.h>
+#include <string>
 
 int main(void)
 {
-    char *input;
+    std::string input;
     std::string str1= "git add *; git commit -m \"";
     std::string str2= "\"; git push";
+    std::string cmd;
     char type[1];
     std::cout << "\033[1;33m╭────Auto-push\n╰─$ \033[0m [1] - Manual push, only enter commit\n     [2] - Automatic push every minute\n\n pick type of auto github push : ";
     std::cin.get(type, 2);
@@ -14,8 +16,10 @@ int main(void)
         while(std::cin.good())
         {
             std::cout << "\n\033[1;33m╭────Auto-push\n╰─$ \033[0mEnter commit message \033[1;33m: \033[0m";
-            std::getline(input, 200, '\n');
-            std::cout << input;
+            std::getline (std::cin,input);
+            cmd = (str1 + input + str2);
+            std::cout << cmd;
+
             std::system((str1 + input + str2).c_str());
         }
     }
