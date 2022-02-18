3dda78ff5c8aa875986bb99cd10999478ea13224
Ophen <itsOphen@gmail.com>
2022-02-18 15:13:14 +0100

    fix

--- auto_push.cpp
+++ auto_push.cpp
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-    std::string input;
+    char *input;
+    std::string str1= "git add *; git commit -m \"";
+    std::string str2= "\"; git push";
     char type[1];
     std::cout << "\033[1;33m╭────Auto-push\n╰─$ \033[0m [1] - Manual push, only enter commit\n     [2] - Automatic push every minute\n\n pick type of auto github push : ";
     std::cin.get(type, 2);
@@ -12,8 +14,8 @@ int main(void)
         while(std::cin.good())
         {
             std::cout << "\033[1;33m╭────Auto-push\n╰─$ \033[0mEnter commit message \033[1;33m: \033[0m";
-            std::cin >> input;
-            std::system(("git add *; git commit -m \"" + input + "\"; git push").c_str());
+             scanf("%s", input);
+            std::system((str1 + input + str2).c_str());
         }
     }
     else if (type[0] == '2')
