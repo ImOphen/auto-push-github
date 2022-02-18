6a64be30e06a215608d87d9ff9c91f4e0f8d777f
Ophen <itsOphen@gmail.com>
2022-02-18 15:30:08 +0100

    rm a.out

--- auto_push.cpp
+++ auto_push.cpp
@@ -5,9 +5,6 @@
 int main(void)
 {
     std::string input;
-    std::string str1= "git add *; git commit -m \"";
-    std::string str2= "\"; git push";
-    std::string cmd;
     std::string type;
     std::cout << "\033[1;33m╭────Auto-push\n╰─$ \033[0m [1] - Manual push, only enter commit\n     [2] - Automatic push every minute\n\n pick type of auto github push : ";
     std::getline (std::cin,type);
@@ -17,10 +14,7 @@ int main(void)
         {
             std::cout << "\n\033[1;33m╭────Auto-push\n╰─$ \033[0mEnter commit message \033[1;33m: \033[0m";
             std::getline (std::cin,input);
-            cmd = (str1 + input + str2);
-            std::cout << cmd;
-
-            std::system((str1 + input + str2).c_str());
+            std::system(("git add *; git commit -m \"" + input + "\"; git push").c_str());
         }
     }
     else if (type[0] == '2')
