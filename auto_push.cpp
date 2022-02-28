5fddb56c92c4c6e0523cfe5931df97ba4227ae08
Ophen <itsOphen@gmail.com>
2022-02-28 19:38:16 +0100

    added new feature

--- auto_push.cpp
+++ auto_push.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <string>
 
@@ -7,7 +8,8 @@ int main(void)
 	std::string input;
 	int minutes;
 	std::string type;
-	std::cout << "\033[1;33m╭────Auto-push\n╰─$ \033[0m [1] - Manual push, only enter commit\n     [2] - Automatic push every N minute(s) with random commit messages\n\n pick type of auto github push : ";
+	std::cout << "\033[1;33m╭────Auto-push\n╰─$ \033[0m [1] - Manual push, only enter commit\n     [2] - Automatic push every N minute(s) with random commit messages\n\
+     [3] - Automatic push of the tracked file when a change occurs, with random commits\n\n pick type of auto github push : ";
 	std::getline (std::cin,type);
 	if (type[0] == '1')
 	{
@@ -41,6 +43,46 @@ int main(void)
 			std::cout << "\n\033[1;33m $────Auto-push\033[0m\n";
 			std::system("cd ..; git add *; git commit -m \"$(curl -s http://whatthecommit.com/index.txt)\"; git push;");
 			sleep(minutes * 60);
+		}
+	}
+	if (type[0] == '3')
+	{
+		std::cout << "\n\033[1;33m╭────Auto-push\n╰─$ \033[0mEnter file to track and push \033[1;33m: \033[0m";
+		std::getline (std::cin,input);
+		if (!std::cin.good())
+		{
+			std::cout << "Error" << std::endl;
+			return 1;
+		}
+		std::fstream fileStream;
+		fileStream.open("../" + input, std::ios::in);
+		if (!fileStream.is_open())
+		{
+			std::cout << "Invalid file" << std::endl;
+			return(1);
+		}
+		std::string fileBase;
+		std::string fileCompare;
+		std::getline(fileStream, fileBase, '\0');
+		fileStream.close();
+		std::cout << "\n\033[1;33m$───Auto-push" << std::endl << "\033[1;33mWaiting for file change\033[0m\n";
+		while(1)
+		{
+			fileStream.open("../" + input, std::ios::in);
+			if (!fileStream.is_open())
+			{
+				std::cout << "Invalid file" << std::endl;
+				return(1);
+			}
+			std::getline(fileStream, fileCompare, '\0');
+			if (fileBase != fileCompare)
+			{
+				std::system(("cd ..; git add " + input + "; git commit -m \"$(curl -s http://whatthecommit.com/index.txt)\"; git push").c_str());
+				fileBase = fileCompare;
+				std::cout << "\n\033[1;33m$───Auto-push" << std::endl << "\033[1;33mWaiting for file change\033[0m\n";
+			}
+			fileStream.close();
+			sleep(1);
 		}
 	}
 	else
